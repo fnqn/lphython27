db0bc290997060ad3396a54ac8c051d20cba04a7
fnqn <news.frank.zhang@gmail.com>
2016-01-05 10:46:41 +0800

    uploaded learning scripts, and learning notes - leaned 2016/01/05 add high-order functional, map/reduce

---
+++ higherorderfunction_001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import math
+
+print abs(-10)
+abs
+x = abs(-10)
+print(x)
+
+f = abs
+print f
+
+def add(x, y, f):
+    return f(x) + f(y)
+
+print add (-5, 6, abs)
